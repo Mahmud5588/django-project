1ed05917d9781bd9253af7ef1957f102c383238b
Mahmud Ahmedov <150663613+Mahmud5588@users.noreply.github.com>
2025-05-31 12:13:37 +0500

    Update settings.py

--- core_erp/core_erp/settings.py
+++ core_erp/core_erp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xt86pq-t3((7%j^76(^28i0$848iw4is&=&a^b%^hwqp+ac*q5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.60.212.19']
 
 
 # Application definition
