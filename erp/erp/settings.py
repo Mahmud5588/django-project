82f73a2ee181c86c4e71d95383394eab71f04f40
Mahmud Ahmedov <150663613+Mahmud5588@users.noreply.github.com>
2025-06-01 14:46:42 +0500

    Update settings.py

--- erp/erp/settings.py
+++ erp/erp/settings.py
@@ -123,8 +123,17 @@ LOGIN_URL = '/login/'  # URL to redirect to for login
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# Statik fayllarni yig‘ish joyi (production uchun)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Statik fayllar qayerdan yig‘iladi (development/payvand qilishda kerak)
+STATICFILES_DIRS = [
+    BASE_DIR / 'app' / 'static', 
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
