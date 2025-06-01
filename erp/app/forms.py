2af0bddd60bb17d63ef2843200069ff81bda9e62
Mahmud Ahmedov <axmedovmaxmud839@gmail.com>
2025-06-01 16:23:46 +0500

    Initial commit

--- erp/app/forms.py
+++ erp/app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-from .models import Product, Category
+from .model_backup import Product, Category
 
 # Use get_user_model() to avoid potential import conflicts
 User = get_user_model()
