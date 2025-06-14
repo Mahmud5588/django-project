a8de1b61d4d510d89415fd6bacb42366f9cf5c57
Mahmud Ahmedov <150663613+Mahmud5588@users.noreply.github.com>
2025-06-14 17:09:08 +0500

    Update models.py

--- erp/app/models.py
+++ erp/app/models.py
@@ -7,20 +7,11 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField(blank=True)
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_pics')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.avatar:
-            img = Image.open(self.avatar.path)
-            if img.height > 300 or img.width > 300:
-                output_size = (300, 300)
-                img.thumbnail(output_size)
-                img.save(self.avatar.path)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
